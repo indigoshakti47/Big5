b6ca1cc489cc1dac4ae8bb118eb42490ca1323d8
Indigo <indigoshakti47@unseen.is>
2020-12-11 15:12:43 -0500

    aspects

---
+++ big5/src/pages/Aspects.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import Button from '@material-ui/core/Button'
+
+import Question from "../components/Question";
+import questions from "../questions/ASPECTS.json";
+
+//import { v4 as uuidv4 } from "uuid"
+import axios from "axios"
+
+const formatedQuestions = questions.map(question => ({
+	question,
+	selected: null
+}));
+
+export default function Aspects(props) {
+	const [currentIndex, setCurrent] = useState(0);
+	const [selectedQuestions, setSelectedQuestions] = useState(formatedQuestions);
+
+	const handleSelect = (option, index) => {
+		console.log(option, index)
+		const newSelected = selectedQuestions.map((question, i) => {
+			if (i === index) {
+				const newQuestion = { ...question };
+				newQuestion.selected = option;
+				return newQuestion;
+			}
+			return question;
+		});
+		setSelectedQuestions(newSelected);
+		if (currentIndex + 1 < selectedQuestions.length) {
+			setTimeout(() => setCurrent(currentIndex + 1), 100);
+		} else {
+
+			axios({
+				url: '/api/servqual',
+				method: 'post',
+				data: {
+					"servqual": newSelected,
+					"user": localStorage.getItem('testerId'),
+					"username": localStorage.getItem('testerUsername')
+				},
+				headers: {
+					Accept: "application/json",
+					"Content-Type": "application/json",
+				},
+			}).then((response) => {
+					console.log(response);
+					setTimeout(() => props.history.push(`/resultservqual`), 1400);
+			}).catch((error) => {
+					console.log(error);
+			});
+
+			console.log(newSelected)
+		}
+	}
+
+	const currentQuestion = selectedQuestions[currentIndex];
+
+	return <div className="questionary">
+		<Question
+			selected={currentQuestion.selected}
+			question={currentQuestion.question}
+			numOptions={10}
+			onSelect={(option => handleSelect(option, currentIndex))}
+		/>
+		{currentIndex >= 1 && <Button variant="contained" color="secondary" onClick={() => setCurrent(currentIndex - 1)}>Back</Button>}
+	</div>;
+}
--- big5/src/router/routes.js
+++ big5/src/router/routes.js
@@ -1,10 +1,11 @@
 import Login from '../pages/Login';
 import Servqual from '../pages/Servqual'
 import BFI from '../pages/BFI';
-import ResultBfi from '../pages/resultBfi/ResultBfi';
+import ResultBfi from '../pages/resultBfi/resultBfi';
 import ResultServqual from '../pages/resutlServqual/ResultServqual';
 import PersonalForm from '../pages/PersonalForm';
 import Users from '../pages/Users'
+import Aspects from '../pages/Aspects'
 
 const ROUTES = [
   {
@@ -28,6 +29,14 @@ const ROUTES = [
     component: PersonalForm,
     auth: true,
   },
+
+  {
+    path: '/bfi',
+    key: 'BFI',
+    exact: true,
+    component: BFI,
+    auth: true,
+  },
   {
     path: ['/servqual'], 
     key: 'SERVQUAL',
@@ -36,10 +45,10 @@ const ROUTES = [
     auth: true,
   },
   {
-    path: '/bfi',
-    key: 'BFI',
+    path: ['/aspects'], 
+    key: 'ASPECTS',
     exact: true,
-    component: BFI,
+    component: Aspects,
     auth: true,
   },
   {
