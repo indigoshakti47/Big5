7fb2dd6eb767a310b6a7961d0490b207d16fb726
Oscar Alonso <oscaralsa@unisabana.edu.co>
2020-12-12 08:31:36 -0500

    comparison last

--- big5/src/pages/ComparisonResult/ComparisonResult.js
+++ big5/src/pages/ComparisonResult/ComparisonResult.js
@@ -128,6 +128,33 @@ const ComparisonResult = (props) => {
   console.log(aspects1, aspects2);
   return !mount ? <p></p> : (
     <div className="elements">
+      <div className="elements__first">
+        <div className="elements__first__userContainer">
+          <h2>User 1</h2>
+          <br></br>
+          <div><strong>Name: </strong> { user1.name }</div>
+          <div><strong>Age: </strong>{ user1.age }</div>
+          <div><strong>Educational Level: </strong>{ user1.educationalLevel }</div>
+          <div><strong>Gender: </strong>{ user1.gender }</div>
+          <div><strong>Stratum: </strong>{ user1.stratum }</div>
+          <div><strong>Hobbies: </strong>{ user1.hobbies }</div>
+          <div><strong>Nationality: </strong>{ user1.nationality }</div>
+          <div><strong>Profession: </strong>{ user1.profession }</div>
+        </div>
+
+        <div className="elements__first__userContainer">
+          <h2>User 2</h2>
+          <br></br>
+          <div><strong>Name: </strong> { user2.name }</div>
+          <div><strong>Age: </strong>{ user2.age }</div>
+          <div><strong>Educational Level: </strong>{ user2.educationalLevel }</div>
+          <div><strong>Gender: </strong>{ user2.gender }</div>
+          <div><strong>Stratum: </strong>{ user2.stratum }</div>
+          <div><strong>Hobbies: </strong>{ user2.hobbies }</div>
+          <div><strong>Nationality: </strong>{ user2.nationality }</div>
+          <div><strong>Profession: </strong>{ user2.profession }</div>
+        </div>
+      </div>
       <BFIComparison bfi1={bfi1} bfi2={bfi2} />
       <ServqualComparison servqual1={srv1} servqual2={srv2} />
       <AspectComparison aspects={aspects1} aspects2={aspects2} name1={user1.name} name2={user2.name} />
